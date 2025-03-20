deb21cf826a2a6d7c22c133183e0cd9fa68715aa
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2025-03-19 22:04:00 -0400

    initial commit

---
+++ transform/udf.sql
@@ -0,0 +1,22 @@
+USE ROLE accountadmin;
+USE WAREHOUSE compute_wh;
+USE DATABASE tasty_bytes;
+
+/* user defined functions */
+-- converts fareinheit to celcius
+CREATE OR REPLACE FUNCTION tasty_bytes.analytics.fahrenheit_to_celsius(temp_f NUMBER(35,4))
+  RETURNS NUMBER(35,4)
+  AS
+  $$
+    (temp_f - 32) * (5/9)
+  $$
+;
+
+-- converts inch to millimeters
+CREATE OR REPLACE FUNCTION tasty_bytes.analytics.inch_to_millimeter(inch NUMBER(35,4))
+  RETURNS NUMBER(35,4)
+  AS
+  $$
+    inch * 25.4
+  $$
+;
